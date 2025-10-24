516409f564fd2f106e4a4e78eeddc45a92558c02
craig <craig.fed@bridgers.finance>
2025-10-23 21:43:57 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors()); // Allow all origins (not recommended for production)
+app.use(express.json());
+app.use("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}}`));
